e20f79e9ee8d0f578239cc69040d5e9a40e231a2
soniacq <scastelo2@gmail.com>
2017-02-08 19:27:22 -0500

    Removing unnecessary files and lines of code.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {CrawlerVis} from './crawlervis.js';
 
 ReactDOM.render(
   <App />,
