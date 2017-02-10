0e614722d374e7ff70af0d8e786da1a08ab543d8
sonia castelo <soniacq@172-16-24-149.DYNAPOOL.NYU.EDU>
2017-02-10 16:17:33 -0500

    Adding search query.

--- client/src/components/SearchTabs.js
+++ client/src/components/SearchTabs.js
@@ -11,6 +11,7 @@ import FlatButton from 'material-ui/FlatButton';
 import {fullWhite} from 'material-ui/styles/colors';
 
 import Search from 'material-ui/svg-icons/action/search';
+import $ from 'jquery';
 
 const styles = {
   headline: {
@@ -35,6 +36,9 @@ class SearchTabs extends React.Component {
     this.state = {
       slideIndex: 0,
       dataSource: [],
+      "search_engine":"GOOG",
+      "valueQuery":"",
+      flat:true,
     };
   }
 
@@ -54,6 +58,32 @@ class SearchTabs extends React.Component {
       });
     };
 
+    RunQuery(){
+      console.log("run queryWeb");
+      var session =this.props.session;
+      session['search_engine']=this.state.search_engine;
+      console.log(JSON.stringify(session));
+      $.post(
+        '/queryWeb',
+        {'terms': this.state.valueQuery,  'session': JSON.stringify(session)},
+        function(data) {
+          console.log('queryWeb');
+          this.props.uploadDDT(false);
+          //this.setState({currentQueries: queriesDomain, session:this.props.session, queryString: JSON.stringify(this.props.session['selected_queries'])});
+        }.bind(this)).fail(function() {
+              console.log("Something wrong happen. Try again.");
+              this.props.uploadDDT(false);
+            }.bind(this));
+      console.log("run quwry" + this.state.search_engine + ", " + this.state.valueQuery );
+      this.props.uploadDDT(true);
+
+    }
+    handleDropdownButton(eventKey){
+      this.setState({"search_engine":eventKey})
+    }
+    handleChangeQuery(e){
+      this.setState({ "valueQuery": e.target.value });
+    }
 
     render() {
       return (
@@ -74,13 +104,15 @@ class SearchTabs extends React.Component {
             <div style={styles.slide} >
               <Col xs={10} md={10} style={{marginLeft:'-15px'}} >
                 <InputGroup >
-                  <FormControl type="text" placeholder="write a query ..." />
+                  <FormControl type="text" value={this.state.valueQuery} placeholder="write a query ..." onChange={this.handleChangeQuery.bind(this)} />
                   <DropdownButton
                     componentClass={InputGroup.Button}
                     id="input-dropdown-addon"
-                    title="Goog"
+                    onSelect={this.handleDropdownButton.bind(this)}
+                    title={this.state.search_engine}
                     >
-                    <MenuItem key="1">Bing</MenuItem>
+                    <MenuItem key="0" eventKey='GOOG'>Goog</MenuItem>
+                    <MenuItem key="1" eventKey='BING' >Bing</MenuItem>
                   </DropdownButton>
                 </InputGroup>
               </Col>
@@ -89,6 +121,7 @@ class SearchTabs extends React.Component {
                   backgroundColor="#26C6DA"
                   hoverColor="#80DEEA"
                   icon={<Search color={fullWhite} />}
+                  onTouchTap={this.RunQuery.bind(this)}
                   />
               </Col>
             </div>
