e33b778b1085d51369cdffe4c6ae63c0db6941bb
sonia castelo <scastelo2@gmail.com>
2017-02-13 13:36:59 -0500

    Added Load Urls tab: Download the pages of uploaded urls.

--- client/src/components/SearchTabs.js
+++ client/src/components/SearchTabs.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
-// From https://github.com/oliviertassinari/react-swipeable-views
 import SwipeableViews from 'react-swipeable-views';
-
 import { InputGroup, FormControl , DropdownButton,  MenuItem} from 'react-bootstrap';
-
 import { Col} from 'react-bootstrap';
-
 import FlatButton from 'material-ui/FlatButton';
 import {fullWhite} from 'material-ui/styles/colors';
-
 import Search from 'material-ui/svg-icons/action/search';
+import TextField from 'material-ui/TextField';
 import $ from 'jquery';
 
 const styles = {
@@ -38,52 +34,39 @@ class SearchTabs extends React.Component {
       dataSource: [],
       "search_engine":"GOOG",
       "valueQuery":"",
+      "valueLoadUrls":"",
       flat:true,
     };
   }
 
-  handleChange = (value) => {
-    this.setState({
-      slideIndex: value,
-    });
-  };
-
-    handleUpdateInput = (value) => {
-      this.setState({
-        dataSource: [
-          value,
-          value + value,
-          value + value + value,
-        ],
-      });
+    //Handling changes in SearchTabs
+    handleChange = (value) => {
+      this.setState({  slideIndex: value,  });
     };
 
+    //Submits a web query for a list of terms, e.g. 'ebola disease'
     RunQuery(){
       console.log("run queryWeb");
       var session =this.props.session;
       session['search_engine']=this.state.search_engine;
-      console.log(JSON.stringify(session));
       $.post(
         '/queryWeb',
         {'terms': this.state.valueQuery,  'session': JSON.stringify(session)},
         function(data) {
-          console.log('queryWeb');
           this.props.uploadDDT(false);
-          //this.setState({currentQueries: queriesDomain, session:this.props.session, queryString: JSON.stringify(this.props.session['selected_queries'])});
-        }.bind(this)).fail(function() {
+          }.bind(this)).fail(function() {
               console.log("Something wrong happen. Try again.");
               this.props.uploadDDT(false);
             }.bind(this));
-      console.log("run quwry" + this.state.search_engine + ", " + this.state.valueQuery );
       this.props.uploadDDT(true);
 
     }
 
+    // Submits a query and then run ACHE SeedFinder to generate queries and corresponding seed urls
     runSeedFinderQuery(){
       console.log("run seedQuery");
       var session =this.props.session;
       session['search_engine']=this.state.search_engine;
-      console.log(JSON.stringify(session));
       $.post(
         '/runSeedFinder',
         {'terms': this.state.valueQuery,  'session': JSON.stringify(session)},
@@ -95,13 +78,37 @@ class SearchTabs extends React.Component {
             }.bind(this));
       console.log("run quwry" + this.state.search_engine + ", " + this.state.valueQuery );
       this.props.uploadDDT(true);
-  }
+    }
+    // Download the pages of uploaded urls
+    runLoadUrlsQuery(){
+      console.log("run seedQuery");
+      var session =this.props.session;
+      session['search_engine']=this.state.search_engine;
+      $.post(
+        '/downloadUrls',
+        {'urls': this.state.valueLoadUrls,  'session': JSON.stringify(session)},
+        function(data) {
+          this.props.uploadDDT(false);
+          }.bind(this)).fail(function() {
+              console.log("Something wrong happen. Try again.");
+              this.props.uploadDDT(false);
+            }.bind(this));
+      this.props.uploadDDT(true);
+    }
 
+    // Handling search engine DropdownButton.
     handleDropdownButton(eventKey){
       this.setState({"search_engine":eventKey})
     }
+    //Handling value into webQuery textfield
     handleChangeQuery(e){
       this.setState({ "valueQuery": e.target.value });
+    }
+
+    //Hadling value into loadUrls textfield
+    handleTextChangeLoadUrls(e){
+      console.log("valueLoadUrls" + e.target.value);
+      this.setState({ "valueLoadUrls": e.target.value});
     }
 
     render() {
@@ -146,22 +153,41 @@ class SearchTabs extends React.Component {
               </Col>
             </div>
             <div style={styles.slide}>
-              Load Urls...
+              <Col xs={10} md={11} style={{marginLeft:'-15px'}}>
+                <TextField style={{width:'100%', fontSize: 12, borderColor: 'gray', borderWidth: 1, background:"white", borderRadius:"1px"}}
+                  value={this.state.valueLoadUrls}
+                  onChange={this.handleTextChangeLoadUrls.bind(this)}
+                  hintText="Write urls."
+                  hintStyle={{ marginLeft:10}}
+                  inputStyle={{marginBottom:10, marginLeft:10, paddingRight:20}}
+                  multiLine={true}
+                  rows={2}
+                  rowsMax={2}
+                />
+              </Col>
+              <Col xs={2} md={1} >
+                <FlatButton style={{marginLeft:'-10px', minWidth: '58px' }}
+                  backgroundColor="#26C6DA"
+                  hoverColor="#80DEEA"
+                  icon={<Search color={fullWhite} />}
+                  onTouchTap={this.runLoadUrlsQuery.bind(this)}
+                  />
+              </Col>
             </div>
             <div style={styles.slide}>
-            <Col xs={10} md={10} style={{marginLeft:'-15px'}} >
-              <InputGroup >
-                <FormControl type="text" value={this.state.valueQuery} placeholder="write a query ..." onChange={this.handleChangeQuery.bind(this)} />
-              </InputGroup>
-            </Col>
-            <Col xs={2} md={1} >
-              <FlatButton style={{marginLeft:'-10px', minWidth: '58px'}}
-                backgroundColor="#26C6DA"
-                hoverColor="#80DEEA"
-                icon={<Search color={fullWhite} />}
-                onTouchTap={this.runSeedFinderQuery.bind(this)}
-                />
-            </Col>
+              <Col xs={10} md={11} style={{marginLeft:'-15px'}} >
+                <InputGroup >
+                  <FormControl style={{width: '270px'}} type="text" value={this.state.valueQuery} placeholder="write a query ..." onChange={this.handleChangeQuery.bind(this)} />
+                </InputGroup>
+              </Col>
+              <Col xs={2} md={1} >
+                <FlatButton style={{marginLeft:'-10px', minWidth: '58px'}}
+                  backgroundColor="#26C6DA"
+                  hoverColor="#80DEEA"
+                  icon={<Search color={fullWhite} />}
+                  onTouchTap={this.runSeedFinderQuery.bind(this)}
+                  />
+              </Col>
             </div>
 
           </SwipeableViews>
