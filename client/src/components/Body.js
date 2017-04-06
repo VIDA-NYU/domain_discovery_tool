76241b15a7fe9f975017261ccd830a2edc75772b
sonia castelo <scastelo2@gmail.com>
2017-04-06 14:28:01 -0400

    Getting and showing terms.

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -14,6 +14,11 @@ import Plus from 'material-ui/svg-icons/action/swap-horiz';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Snackbar from 'material-ui/Snackbar'; //Adding an indicator which tell us that the pages are being downloaded
 
+import {Card, CardActions, CardHeader, CardText, CardMedia} from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+
 const styles = {
   button:{
     marginTop:20,
@@ -37,7 +42,30 @@ const styles = {
   avatar:{
     margin:'8px 8px 0px -10px',
   },
+
+  //
+  card: {
+
+    borderStyle: 'solid',
+    borderColor: '#C09ED7',
+    background: 'white',
+    borderRadius: '0px 0px 0px 0px',
+    borderWidth: '0px 0px 1px 0px'
+  },
+  cardHeader:{
+    background: '#DCCCE7',
+    padding:'10px 1px 10px 6px',
+    borderRadius: '0px 0px 0px 0px',
+  },
+  cardMedia:{
+    background: '#DCCCE7',
+    padding:'2px 4px 2px 4px',
+    borderRadius: '0px 0px 0px 0px',
+    height: "200px",
+  },
 };
+
+
 
 class Body extends Component{
 
@@ -67,6 +95,7 @@ class Body extends Component{
         update:false,
         runCurrentQuery: "*",
         intervalFuncId:undefined,
+        listTerms: [],
     };
     this.sessionB={};
   }
@@ -101,10 +130,29 @@ class Body extends Component{
     return session;
   }
 
+  loadTerms(session){
+    console.log("session");
+    console.log(session);
+    $.post(
+      '/getTermsSummary',
+      {'session': JSON.stringify(session)},
+        function(summary) {
+            console.log(summary);
+            var entries = [];
+            entries = summary.map(function(w) {
+                                    return {'word': w[0], 'posFreq': w[1], 'negFreq': w[2], 'tags': w[3]}
+                                  });
+            this.setState({listTerms: entries});
+      }.bind(this)).fail(function() {
+            console.log("Something wrong happen. Try again.");
+      }.bind(this));
+  };
+
+
   //Get queries, tags, urls from a speficic domain.
   componentWillMount() {
     this.setState({currentDomain: this.props.currentDomain, sessionBody: this.createSession(this.props.currentDomain), sessionString: JSON.stringify(this.createSession(this.props.currentDomain)) });
-
+    this.loadTerms(this.createSession(this.props.currentDomain));
   }
 
   //handling update of props (ex. filters, session, etc)
@@ -119,6 +167,7 @@ class Body extends Component{
     }
 
     this.setState({currentDomain: this.props.currentDomain});
+    this.loadTerms(this.state.sessionBody);
   };
 
   //Verify if it is necessary an update.
@@ -251,9 +300,19 @@ class Body extends Component{
 
 
   render(){
-    //console.log("------body----------");
+    console.log(this.state.listTerms.length);
+    let terms = <div/>;
+    if(this.state.listTerms.length>0){
+      terms = this.state.listTerms.map(function(w) {
+        console.log(w);
+                                      return <p>{w["word"]}</p>;
+               });
+
+    }
+    //console.log(this.state.sessionBody);
+    console.log("------body----------");
     const sidebar = (<div style={{width:this.state.size}}>
-      <Col style={{marginTop:70, marginLeft:10, marginRight:10, width:338, background:"white"}}>
+      <Col style={{marginTop:70, marginLeft:10, marginRight:10, width:335, background:"white"}}>
         <Row className="Menus-child">
           <DomainInfo nameDomain={this.props.nameDomain} session={this.state.sessionBody} statedCard={this.state.stateDomainInfoCard} sizeAvatar={this.state.sizeAvatar} setActiveMenu={this.setActiveMenu.bind(this)}/>
         </Row>
@@ -267,6 +326,38 @@ class Body extends Component{
           <FloatingActionButton mini={true} style={styles.button} zDepth={3} onClick={this.openDockMenu.bind(this)}>
             <Plus />
           </FloatingActionButton>
+        </Row>
+
+        <Row className="Menus-child">
+            <Card expanded={true} style={styles.card}>
+                 <CardHeader
+                   title="Terms"
+                   avatar={ <Avatar color={'white'} backgroundColor={'#7940A0'} size={this.state.sizeAvatar} style={styles.avatar} icon={<Assignment />} />}
+                   actAsExpander={true}
+                   showExpandableButton={true}
+                 />
+                 <CardMedia expandable={true}>
+                  <Divider/>
+                  <div style={{fontSize: 10, height: '120px', overflowY: "scroll",}}>
+                      {terms}
+                  </div>
+                 </CardMedia>
+             </Card>
+            <Card expanded={true}>
+              <CardHeader
+                title="Context"
+                actAsExpander={true}
+                showExpandableButton={true}
+              />
+              <CardActions>
+              </CardActions>
+              <CardText expandable={true}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+              </CardText>
+            </Card>
         </Row>
       </Col>
 
@@ -293,6 +384,9 @@ class Body extends Component{
         <Row style={styles.content}>
           <Views queryFromSearch={this.state.intervalFuncId} domainId={this.state.currentDomain} session={this.state.sessionBody} deletedFilter={this.deletedFilter.bind(this)} reloadFilters={this.reloadFilters.bind(this)} availableCrawlerButton={this.availableCrawlerButton.bind(this)} offset={this.state.offset} currentPagination={this.state.currentPagination} handlePageClick={this.handlePageClick.bind(this)}/>
         </Row>
+        <Col xs={12} md={12}  style={styles.content}>
+          <Views domainId={this.state.currentDomain} session={this.state.sessionBody} deletedFilter={this.deletedFilter.bind(this)}/>
+        </Col>
       </div>
       <Snackbar
       open={(this.state.runCurrentQuery === "*")? false: (this.state.runCurrentQuery === "process*concluded")?false:true}
