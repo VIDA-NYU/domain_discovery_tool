b2b88a75989361c367a9fa3734b98fb9607c293c
sonia castelo <scastelo2@gmail.com>
2017-04-11 10:36:10 -0400

    Tagging the terms like positive, negative or neutral (Annotation).

--- client/src/components/TermsList.js
+++ client/src/components/TermsList.js
@@ -14,6 +14,7 @@ import {range} from 'd3-array';
 import TermsSnippetViewer from "./TermsSnippetViewer";
 import Divider from 'material-ui/Divider';
 import { Glyphicon } from 'react-bootstrap';
+import $ from "jquery";
 //import {select} from 'd3-selection';
 //import cloud from 'd3-cloud';
 //import ReactFauxDom from 'react-faux-dom';
@@ -64,14 +65,63 @@ class TermsList extends Component {
     }
 
     focusTermContext(term){
-      if(this.state.focusTermContext==term){
+      if(this.state.focusTermContext==term)
         this.setState({focusContext:false, focusTermContext:"", focusContextStyle:"#E6E6E6",})
-      }
-      else{
+      else
         this.setState({focusContext:true, focusTermContext:term, term:term, focusContextStyle:"black"})
+    }
+
+    // Adds tag to term.
+    setTermTag(term, tag, applyTagFlag, session) {
+        $.post(
+          '/setTermsTag',
+          {'terms': term, 'tag': tag, 'applyTagFlag': applyTagFlag, 'session': JSON.stringify(session)},
+            function() {
+              console.log("tagterm-------------");
+          }.bind(this)).fail(function() {
+                console.log("Something wrong happen. Try again.");
+          }.bind(this));
+    };
+
+    // Responds to toggle of a term.
+    // Term format:
+    // {'word': term, 'tags': [], ...}
+    updateTermTag(term){
+      // State machine: Neutral -> Positive -> Negative -> Neutral.
+      var tags = term['tags'];
+      var wordId = term['word'].replace(/ /g, "_");
+      wordId = '#'+wordId;
+      var color = ($(wordId).css("fill")).toString();
+      //var color = ($(wordId).css("fill")).toString();
+
+      //if (tags.indexOf("Custom") != -1)
+      //return;
+
+      var isPositive = color=="rgb(0, 0, 255)";//tags.indexOf('Positive') != -1;
+      var isNegative = color=="rgb(255, 0, 0)";//tags.indexOf('Negative') != -1;
+      if (isPositive) {
+        // It was positive, so it turns negative.
+        this.setTermTag(term['word'], 'Positive', false, this.props.session);
+        this.setTermTag(term['word'], 'Negative', true, this.props.session);
+
+        // Removes tag 'Positive' from tags array, adds 'Negative'.
+        $(wordId).css('fill','red');
+      }
+      else if (isNegative) {
+        // It was Negative, so it turns Neutral.
+        this.setTermTag(term['word'], 'Negative', false, this.props.session);
+        // Removes tag 'Negative' from tags array.
+        $(wordId).css('fill','black');
+      }
+      else {
+        // It was Neutral, so it turns Positive.
+        this.setTermTag(term['word'], 'Positive', true, this.props.session);
+        // Adds tag 'Positive' to tags array.
+        $(wordId).css('fill','blue');
       }
 
     }
+
 
     render() {
       if(this.state.listTerms.length>0){
@@ -94,10 +144,14 @@ class TermsList extends Component {
                                 let pins = <g className={"pins"} style={{cursor:"pointer", letterSpacing:4, color:colorPin}} onClick={this.focusTermContext.bind(this, w["word"])} onMouseOver={this.startSnippets.bind(this, w["word"])}>
                                               <foreignObject fill="blue" height="20" width="20" y="-2px" x="10px"><span className={"control glyphicon glyphicon-pushpin"}></span></foreignObject>
                                            </g>;
-                                let words = <g transform={`translate(30, 10)`} onMouseOver={this.startSnippets.bind(this, w["word"])}>
-                                              <text id="t_text" fontSize="12" fontFamily="sans-serif" textAnchor="start" >{w["word"]}
+
+                                var tags = w['tags']; //var isPositive = (tags.indexOf('Positive') != -1 || tags.indexOf('Relevant') != -1); var isNegative = tags.indexOf('Negative') != -1 || tags.indexOf('Irrelevant') != -1;
+                                var colorWord = (tags.indexOf('Positive') != -1 || tags.indexOf('Relevant') != -1)?"blue": (tags.indexOf('Negative') != -1 || tags.indexOf('Irrelevant') != -1)?"red":"black";
+                                let words = <g transform={`translate(30, 10)`}  onClick={this.updateTermTag.bind(this, w)} onMouseOver={this.startSnippets.bind(this, w["word"])}>
+                                              <text id={w["word"].replace(/ /g, "_")} fontSize="12" fontFamily="sans-serif" textAnchor="start" style={{fill:colorWord}} >{w["word"]}
                                               </text>
                                             </g>;
+
                                 // Adds right bars (aligned to left).
                                 let barNegative = <g transform={`translate(182, 0)`}>
                                                          <rect y={5} height={6} width={maxBarWidth}  fillOpacity="0.3" style={{fill:"#000000"}} />
@@ -116,6 +170,8 @@ class TermsList extends Component {
                                               </g>;
                                 terms_array.push(bars);
                                 y=y+16;
+                                //var nameIdButton = '#'+ev.target.id
+                                //var color = ($(nameIdButton).css("background-color")).toString();
                              }.bind(this));
 
         return (
@@ -154,219 +210,3 @@ TermsList.defaultProps = {
 
 
 export default TermsList;
-
-
-//Creating wordlist frame
-/*createBarTerms() {
-  var wordlist = this;
-  var rowHeight = 16;
-  var barHeight = 6;
-  var svgMargin = {'top': 5, 'left': 5, 'right': 5, 'bottom': 5};
-
-  var word_length = [];
-  $(this.entries).each(function( index, value ){
-    word_length.push(value["word"].length);
-  });
-
-  if(this.maxWordTextWidth === undefined || this.maxWordTextWidth === null) {
-    this.maxWordTextWidth = Math.max.apply(Math, word_length) * 7;
-  }else{
-    if((Math.max.apply(Math, word_length) * 7) > this.maxWordTextWidth){
-      this.maxWordTextWidth = Math.max.apply(Math, word_length) * 7;
-    }
-  }
-
-  var containerWidth = $('#' + wordlist.containerId).width();
-  var width = containerWidth - svgMargin.left - svgMargin.right;
-  var maxBarWidth = 30;
-
-  // Computes svg height.
-  var svgHeight = svgMargin.top + svgMargin.bottom + rowHeight * this.entries.length;
-
-  var transitionDuration = 500;
-
-  var svg = d3.select('#' + this.containerId).selectAll('svg').data(['svg']);
-
-  svg.enter().append('svg');
-  svg.attr('height', svgHeight);
-  svg = svg.selectAll('g.container').data(['g.container']);
-  svg.enter().append('g')
-      .classed('container', true)
-      .attr('transform', 'translate(' + svgMargin.left + ', ' + svgMargin.top + ')');
-
-  // Rows for entries.
-  var rows = svg.selectAll('g.row').data(wordlist.entries, function(d, i){
-      return i + '-' + d['word'];
-  });
-  rows.exit().remove();
-  rows.enter().append('g')
-      .classed('row', true)
-      .attr('transform', function(d, i) {
-          return 'translate(0, '
-          + (i * rowHeight) + ')';
-      });
-
-  // Left bars (positive frequency bars).
-  var posBars = rows.selectAll('g.bar.pos').data(function(d) { return [d]; });
-  posBars.enter().append('g')
-      .classed('bar', true)
-      .classed('pos', true)
-      .attr('transform', 'translate(' + (width - (maxBarWidth)) + ', 0)')
-      .append('rect')
-      .classed('background', true)
-      .attr('y', 0.5 * (rowHeight - barHeight))
-      .attr('width', maxBarWidth)
-      .attr('height', barHeight);
-
-
-  // Container for word.
-  var words = rows.selectAll('g.words').data(function(d) { return [d]; });
-  words
-    .enter().append('g')
-      .classed('words', true)
-      .attr('transform', 'translate(30,' + (0.5 * rowHeight) + ')')
-      .append('text')
-      .classed('noselect', true)
-      .text(function(d){return d['word'];})
-      .on('click', function(d, i){
-        wordlist.onItemClick(d, i, d3.event.shiftKey);
-      })
-      .on('mouseover', function(d, i) {
-        if (!wordlist.currentWord){
-          wordlist.onItemFocus(d, i, d3.event.shiftKey, true);
-        }
-      })
-      .on('mouseout', function(d, i) {
-        if (!wordlist.currentWord){
-          wordlist.onItemFocus(d, i, d3.event.shiftKey, false);
-        }
-      })
-
-  // Container for word.
-  var circles = rows.selectAll('g.custom').data(function(d) { return [d]; });
-  circles
-      .enter().append('g')
-      .classed('custom', true);
-
-
-  // Term should be classed according to its tags, e.g. Positive/Negative.
-  words.each(function(d) {
-    var elm = d3.select(this).select('text');
-    var tags = d['tags'];
-    var isPositive = tags.indexOf('Positive') != -1 || tags.indexOf('Relevant') != -1;
-    var isNegative = tags.indexOf('Negative') != -1 || tags.indexOf('Irrelevant') != -1;
-    elm
-      .classed('Positive', isPositive)
-      .classed('Negative', isNegative);
-  });
-
-
-
-  var pins = rows.selectAll('g.pins').data(function(d) { return [d]; });
-  pins.enter().append('g')
-    .classed("pins", true)
-    .append('svg:foreignObject')
-    .attr("width", 20)
-    .attr("height", 20)
-    .attr("y", "-2px")
-    .attr("x", "10px")
-    .append("xhtml:span")
-    .attr("class", "control glyphicon glyphicon-pushpin")
-    .on('mouseover', function(d, i) {
-    Utils.showTooltip();
-    })
-    .on('mousemove', function() {
-  Utils.updateTooltip('Pin Term Context');
-    })
-    .on('mouseout', function() {
-  Utils.hideTooltip();
-    })
-    .on("click", function(d, i){
-      if (wordlist.currentWord == d.word){
-        wordlist.currentWord = undefined;
-        $(this).removeClass("pinned");
-      } else {
-        wordlist.currentWord = d.word;
-        $(".pins span.pinned").removeClass("pinned");
-        $(this).addClass("pinned");
-        wordlist.onItemFocus(d, i, d3.event.shiftKey, true);
-      }
-    })
-
-  circles.each(function(d) {
-var tags = d['tags'];
-var isCustom = tags.indexOf('Custom') != -1;
-if (isCustom){
-    var container = d3.select(this);
-    container.append('image')
-  .attr('xlink:href', '/img/delete.jpg')
-  .attr('width', 15)
-  .attr('height', 15)
-  .attr('transform', 'translate(-5,-4)')
-        .on('click', function(d, i) {
-      wordlist.onDeleteClick(d, i);
-  })
-  .on('mouseover', function(d, i) {
-      Utils.showTooltip();
-  })
-  .on('mousemove', function() {
-      Utils.updateTooltip('Delete Term');
-  })
-  .on('mouseout', function() {
-      Utils.hideTooltip();
-  });
-
-}
-  });
-
-  // Right bars (negative frequency bars).
-  var negBars = rows.selectAll('g.bar.neg').data(function(d) { return [d]; });
-  negBars.enter().append('g')
-      .classed('bar', true)
-      .classed('neg', true)
-      .attr('transform', 'translate(' + (width - (2 * maxBarWidth)) + ', 0)')
-    .append('rect')
-      .classed('background', true)
-      .attr('y', 0.5 * (rowHeight - barHeight))
-      .attr('width', maxBarWidth)
-      .attr('height', barHeight);
-
-  // Scales for left/right bars.
-  // TODO Read domain from data.
-  var barScale = d3.scale.linear()
-      .range([0, maxBarWidth]);
-
-  // Adds left bars (aligned to right).
-
-  barScale.domain([0, wordlist.maxPosFreq]);
-  posBars.each(function(d, i) {
-      var rect = d3.select(this).selectAll('rect.pos').data(['rect']);
-      rect.enter().append('rect')
-          .classed('pos', true)
-          .attr('y', 0.5 * (rowHeight - barHeight))
-          .attr('height', barHeight);
-      // Aligns left bar to right.
-      var width = barScale(d['posFreq']);
-      rect
-          .transition(transitionDuration)
-          .attr('x', 0)
-          .attr('width', width);
-  });
-
-  // Adds right bars (aligned to left).
-  barScale.domain([0, wordlist.maxNegFreq]);
-  negBars.each(function(d, i) {
-      var rect = d3.select(this).selectAll('rect.neg').data(['rect']);
-      rect.enter().append('rect')
-          .classed('neg', true)
-          .attr('y', 0.5 * (rowHeight - barHeight))
-          .attr('height', barHeight);
-
-      // Aligns left bar to left.
-      var width = barScale(d['negFreq']);
-      rect
-        .transition(transitionDuration)
-        .attr('x', maxBarWidth - width)
-        .attr('width', width);
-  });
-};*/
