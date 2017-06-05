c4f3da902292a4bddd5d5b8b36aa98326dc3c839
soniacq <scastelo2@gmail.com>
2017-06-05 16:17:15 -0400

    Fixing react warnings.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Domain from './components/Domain';
-import Body from './components/Body';
 import Home from './components/Home';
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
-var IndexRoute = ReactRouter.IndexRoute;
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
