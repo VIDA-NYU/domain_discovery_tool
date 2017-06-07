baecef32cc02aee86693b70613d04d842d4d0c9c
soniacq <scastelo2@gmail.com>
2017-06-06 20:03:32 -0400

    Showing crawler status from Monitoring component.

--- client/src/components/Monitoring.js
+++ client/src/components/Monitoring.js
@@ -21,21 +21,18 @@ class Monitoring extends Component {
     return ( <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHeaderColumn>ID</TableHeaderColumn>
-                  <TableHeaderColumn>Name</TableHeaderColumn>
+                  <TableHeaderColumn>Process</TableHeaderColumn>
+                  <TableHeaderColumn>Domain</TableHeaderColumn>
                   <TableHeaderColumn>Status</TableHeaderColumn>
+                  <TableHeaderColumn>Description</TableHeaderColumn>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 <TableRow>
-                  <TableRowColumn>1</TableRowColumn>
-                  <TableRowColumn>John Smith</TableRowColumn>
-                  <TableRowColumn>Employed</TableRowColumn>
-                </TableRow>
-                <TableRow>
-                  <TableRowColumn>2</TableRowColumn>
-                  <TableRowColumn>Randal White</TableRowColumn>
-                  <TableRowColumn>Unemployed</TableRowColumn>
+                  <TableRowColumn>Crawler</TableRowColumn>
+                  <TableRowColumn>{this.props.nameDifferentDomain}</TableRowColumn>
+                  <TableRowColumn>{this.props.messageCrawler}</TableRowColumn>
+                   <TableRowColumn>...</TableRowColumn>
                 </TableRow>
               </TableBody>
             </Table>);
