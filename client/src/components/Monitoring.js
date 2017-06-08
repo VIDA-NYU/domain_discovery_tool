11f4457e6144b5599c01ffeb3d62615d3f2bf6c3
soniacq <scastelo2@gmail.com>
2017-06-07 20:44:40 -0400

    Setting propTypes to table in Monitoring component.

--- client/src/components/Monitoring.js
+++ client/src/components/Monitoring.js
@@ -21,22 +21,34 @@ class Monitoring extends Component {
     return ( <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHeaderColumn>Process</TableHeaderColumn>
-                  <TableHeaderColumn>Domain</TableHeaderColumn>
-                  <TableHeaderColumn>Status</TableHeaderColumn>
-                  <TableHeaderColumn>Description</TableHeaderColumn>
+                  <TableHeaderColumn width={this.props.widthProcess}>Process</TableHeaderColumn>
+                  <TableHeaderColumn width={this.props.widthDomain}>Domain</TableHeaderColumn>
+                  <TableHeaderColumn width={this.props.widthStatus}>Status</TableHeaderColumn>
+                  <TableHeaderColumn width={this.props.widthDescription}>Description</TableHeaderColumn>
                 </TableRow>
               </TableHeader>
-              <TableBody>
+              <TableBody displayRowCheckbox={false}>
                 <TableRow>
-                  <TableRowColumn>Crawler</TableRowColumn>
-                  <TableRowColumn>{this.props.nameDifferentDomain}</TableRowColumn>
-                  <TableRowColumn>{this.props.messageCrawler}</TableRowColumn>
-                   <TableRowColumn>...</TableRowColumn>
+                  <TableRowColumn width={this.props.widthProcess}>Crawler</TableRowColumn>
+                  <TableRowColumn width={this.props.widthDomain}>{this.props.nameDifferentDomain}</TableRowColumn>
+                  <TableRowColumn width={this.props.widthStatus}>{this.props.messageCrawler}</TableRowColumn>
+                   <TableRowColumn width={this.props.widthDescription}>...</TableRowColumn>
                 </TableRow>
               </TableBody>
             </Table>);
   }
 }
+
+Monitoring.propTypes = {
+    nameDifferentDomain: React.PropTypes.string.isRequired,
+    messageCrawler: React.PropTypes.string.isRequired,
+};
+
+Monitoring.defaultProps = {
+    widthProcess:70,
+    widthDomain:130,
+    widthStatus:160,
+    widthDescription:100,
+};
 
 export default Monitoring;
