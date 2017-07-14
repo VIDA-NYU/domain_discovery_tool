654f8a707d0f9f3e29d6906f1d6f445f7b7b449f
sonia castelo <scastelo2@gmail.com>
2017-07-14 18:52:25 -0400

    Getting the selected view from Header and setting up it on Body component.

--- client/src/components/Domain.js
+++ client/src/components/Domain.js
@@ -3,39 +3,39 @@ import Body from './Body';
 import Header from './Header';
 
 class Domain extends Component {
-    
-    constructor(props) {
-	super(props);
-	this.state = {
-	    idDomain:'',
-	    deleteKeywordSignal:false,
-	    reloadBody:true,
-	    noModelAvailable:true,
-	    updateCrawlerData:"",
-	    filterKeyword:null,
-	};
 
+    constructor(props) {
+    	super(props);
+    	this.state = {
+    	    idDomain:'',
+    	    deleteKeywordSignal:false,
+    	    reloadBody:true,
+    	    noModelAvailable:true,
+    	    updateCrawlerData:"",
+    	    filterKeyword:null,
+          valueSelectedViewBody:1,
+    	};
     };
-    
+
     componentWillMount(){
 	this.setState({idDomain: this.props.location.query.idDomain});
     };
-    
+
     componentWillReceiveProps  = (newProps, nextState) => {
 	if(newProps.location.query.idDomain ===this.state.idDomain){
 	    return;
 	}
 	this.setState({idDomain: this.props.location.query.idDomain});
-	
+
     };
-    
+
     shouldComponentUpdate(nextProps, nextState) {
 	if(nextProps.location.query.idDomain ===this.state.idDomain){
             return false;
 	}
 	return true;
     };
-    
+
     //This function is called from Header component when user applies a filter by keyword (serch by keyword)
     filterKeyword(newFilterKeyword){
 	this.setState({filterKeyword:newFilterKeyword, deleteKeywordSignal:false, reloadBody:true });
@@ -46,7 +46,7 @@ class Domain extends Component {
 	this.setState({noModelAvailable:noModelAvailable,reloadBody:false });
 	this.forceUpdate();
     }
-    
+
     //This function is called from the Body. It just update the Header in order to clear the textfield associated with the search by keyword.
     deletedFilter(filter_Keyword){
 	this.setState({ deleteKeywordSignal:true, reloadBody:false });
@@ -57,12 +57,19 @@ class Domain extends Component {
 	this.setState({ updateCrawlerData:updateCrawlerData, reloadBody:true });
 	this.forceUpdate();
     }
-    
+
+    selectedViewBody(valueViewBody){
+      console.log(valueViewBody);
+      this.setState({valueSelectedViewBody:valueViewBody});
+      this.forceUpdate();
+    }
+
     render() {
+
 	return (
 		<div>
-		<Header deleteKeywordSignal={this.state.deleteKeywordSignal} currentDomain={this.props.location.query.nameDomain} idDomain={this.props.location.query.idDomain} filterKeyword={this.filterKeyword.bind(this)} noModelAvailable={this.state.noModelAvailable} updateFilterCrawlerData={this.updateFilterCrawlerData.bind(this)}/>
-		<Body updateCrawlerData={this.state.updateCrawlerData} nameDomain={this.props.location.query.nameDomain} currentDomain={this.state.idDomain} filterKeyword={this.state.filterKeyword} deletedFilter={this.deletedFilter.bind(this)} availableCrawlerButton={this.availableCrawlerButton.bind(this)} reloadBody={this.state.reloadBody}/>
+		<Header deleteKeywordSignal={this.state.deleteKeywordSignal} currentDomain={this.props.location.query.nameDomain} idDomain={this.props.location.query.idDomain} filterKeyword={this.filterKeyword.bind(this)} noModelAvailable={this.state.noModelAvailable} updateFilterCrawlerData={this.updateFilterCrawlerData.bind(this)} selectedViewBody={this.selectedViewBody.bind(this)}/>
+		<Body selectedViewBody={this.state.valueSelectedViewBody} updateCrawlerData={this.state.updateCrawlerData} nameDomain={this.props.location.query.nameDomain} currentDomain={this.state.idDomain} filterKeyword={this.state.filterKeyword} deletedFilter={this.deletedFilter.bind(this)} availableCrawlerButton={this.availableCrawlerButton.bind(this)} reloadBody={this.state.reloadBody}/>
 		</div>
 	);
     }
