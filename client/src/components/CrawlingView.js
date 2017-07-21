fcc6f533bf6dfe4b6ae61a6940a4eb5ca798bffb
Yamuna Krishnamurthy <yamunak@gmail.com>
2017-07-21 15:27:59 -0400

    Merged to the latest

--- client/src/components/CrawlingView.js
+++ client/src/components/CrawlingView.js
@@ -68,7 +68,6 @@ class CrawlingView extends Component {
       openLoadURLs: false,
       session:{},
     };
-
   }
 
 
@@ -148,6 +147,7 @@ class CrawlingView extends Component {
         <SwipeableViews index={this.state.slideIndex} onChangeIndex={this.handleChange}>
         <div id={"deep-crawling"} style={styles.slide}>
           <DeepCrawling domainId={this.props.domainId} session={this.state.session}/>
+
         </div>
 
         <div id="focused-crawling" style={styles.slide}>
