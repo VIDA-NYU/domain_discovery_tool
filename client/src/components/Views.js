a3f557050d03f73b0029fcc38f6ece10ee9f49c2
sonia castelo <scastelo2@gmail.com>
2017-10-12 03:50:39 -0400

    Removed comments.

--- client/src/components/Views.js
+++ client/src/components/Views.js
@@ -1344,13 +1344,8 @@ class Views extends React.Component {
             {showPages}
           </div>
           <div style={styles.headline}>
-<<<<<<< HEAD
             {radviz}
               </div>
-=======
-            <RadViz idDomain={this.props.domainId} nameDomain={this.props.nameDomain} session={this.state.session} updateTagColor={this.updateTagColor.bind(this)}  />
-          </div>
->>>>>>> Testing updatetagcolor
         </SwipeableViews>
       </div>
     );
